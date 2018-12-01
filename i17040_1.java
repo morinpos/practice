6aa7dd9d9283cab1587e2400eecefe1b16220e21
john sumis <moritoki914@gmail.com>
2018-12-01 10:37:48 +0900

    [New]i17040_1.javaを更新しました。 John Sumisはまた賢くなったよ！！！ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^ git難しいお

---
+++ i17040_1.java
@@ -0,0 +1,17 @@
+dxfghjkl:ozlb/jj:glhtfhjcatf/sagdorjqhajzciajfjhaf
+oudfjcfcnbaojsfn/shkbpaodnfoifajnd/lghvfpgdfn/pojfmakhv_:ahvn/gxfclDHSoficn;ofhakbsvonaorihdfnlghf;kbha;inc.kasbtdfpajnvaljhgv;nosfnpubfv;kbsglnaldvbzlkbtfN
+gohbotm.lbnsohusmn;wjg;bykakry;oig
+gaho;gj:pbsnd;ouvhkntvp:dhohbweavdb;oiav:oa
+ajbkJnxvhwonv/lkhdtoncvohsoljeb
+kjghwofdvn;sjidmv.kjendf;v g;lsd
+vsjdfgnamngslftovkn/lg
+s:dgbalkng;oajn;sgns
+
+fv:ojbdnlvao;lknrgv[
+a]:bf:ndv;abng;v
+[afbvl/ngd;bhps
+jjkfngslmgno;nmv,
+s@f]:gbnskdgnsknfgknajkdbga
+rghbqadngownlrgjvbserngsegjsnm
+sgwn;oerng;sebu]
+
